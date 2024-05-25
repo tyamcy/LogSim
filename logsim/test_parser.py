bba7d03649234ec66a5f47c1556e5f37115a1eeb
Maxwell <maxwell.limingchi@gmail.com>
2024-05-25 15:16:58 +0100

    added test for the parser module

---
+++ logsim/test_parser.py
@@ -0,0 +1,23 @@
+from names import Names
+from devices import Devices
+from network import Network
+from monitors import Monitors
+from scanner import Scanner
+from parser_handler import ParserErrorHandler
+from parse import Parser
+
+PATH = "test_parser_2"
+
+names = Names()
+devices = Devices(names=names)
+network = Network(names=names, devices=devices)
+monitors = Monitors(names=names, devices=devices, network=network)
+scanner = Scanner(names=names, path=PATH)
+parser = Parser(names=names, devices=devices, network=network, monitors=monitors, scanner=scanner)
+
+parser.advance()
+parser.device_list()
+parser.switch_list()
+parser.clock_list()
+parser.monitor_list()
+parser.connect_list()
